845c8170a84d1ef963b0b805a7346de7eb38e44b
AlonsoGonzalezGonzalez <alonso.gonzalezgonzalez16@gmail.com>
2021-10-30 18:52:59 -0500

    yes sir

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,13 @@
+library global;
+
 double light_year = 9500000000000;
-//String? dropDownValue = '';
-//String? secdropDownValue = '';
-//String value = '';
-//String value2 = '';
+String dropDownValue = 'Select';
+String secdropDownValue = 'Select';
+double SuntoMercury = 36000000;
+double SuntoVenus = 67200000;
+double SuntoEarth = 93000000;
+double SuntoMars = 141600000;
+double SuntoJupiter = 483600000;
+double SuntoSaturn = 886700000;
+double SuntoUranus = 1784000000;
+double SuntoNeptune = 2794400000;
--- lib/pages/distance.dart
+++ lib/pages/distance.dart
@@ -1,77 +1,109 @@
 import 'package:flutter/material.dart';
-import 'package:hack_tx_2021/global.dart';
+import 'global.dart';
+import 'problems.dart';
+import 'conversion.dart';
 
-class DistancePage extends StatefulWidget{
+class DistancePage extends StatefulWidget {
   @override
   _DistancePage createState() => _DistancePage();
 }
 
-class _DistancePage extends State<DistancePage>{
-  String dropDownValue ='Select';
-  String secdropDownValue ='Select';
+class _DistancePage extends State<DistancePage> {
+  navigateToConversionPage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ConversionPage()));
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff4C748B),
-      body: Center(
-        child: Column(
+        backgroundColor: Color(0xff4C748B),
+        body: Center(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "Distance in Light Years",
-              style: TextStyle(
-              fontFamily: "Norwester",
-              fontStyle: FontStyle.normal,
-              fontWeight: FontWeight.bold,
-              fontSize: 32,
-              color: Color(0xffFFFFFF))
-          ),
-          Container(
-          child: Row(
-           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           children: [
-              DropdownButton<String>(
-               value: dropDownValue,
-                style: TextStyle(color: Colors.black),
-                 onChanged:(String? newvalue){
-                  setState(() {
-                    dropDownValue = newvalue!;
-                  });
-                },
-                items: <String>['Select','Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'].map<DropdownMenuItem<String>>(
-                  (String value){
-                  return DropdownMenuItem<String>(
-                     value: value,
-                     child: Text(value),
-                  );
-                }).toList(),
-               // hint: Text('Dropdown'),
-               
+            Text("Distance in Light Years",
+                style: TextStyle(
+                    fontFamily: "Norwester",
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                    color: Color(0xffFFFFFF))),
+            Container(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DropdownButton<String>(
+                  value: dropDownValue,
+                  style: TextStyle(color: Colors.black),
+                  onChanged: (String? newvalue) {
+                    setState(() {
+                      dropDownValue = newvalue!;
+                    });
+                  },
+                  items: <String>[
+                    'Select',
+                    'Sun',
+                    'Mercury',
+                    'Venus',
+                    'Earth',
+                    'Mars',
+                    'Jupiter',
+                    'Saturn',
+                    'Uranus',
+                    'Neptune'
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  // hint: Text('Dropdown'),
                 ),
                 DropdownButton<String>(
-                value: secdropDownValue,
-                style: TextStyle(color: Colors.black),
-                items: <String>['Select','Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'].map<DropdownMenuItem<String>>(
-                  (String value){
-                  return DropdownMenuItem<String>(
-                     value: value,
-                     child: Text(value),
-                  );
-                },).toList(),
-                hint: Text('Dropdown'),
-                onChanged: (value){
-                  setState(() {
-                    secdropDownValue = value!;
-                   
-                  });
-                }
-                ),  
+                    value: secdropDownValue,
+                    style: TextStyle(color: Colors.black),
+                    items: <String>[
+                      'Select',
+                      'Sun',
+                      'Mercury',
+                      'Venus',
+                      'Earth',
+                      'Mars',
+                      'Jupiter',
+                      'Saturn',
+                      'Uranus',
+                      'Neptune'
+                    ].map<DropdownMenuItem<String>>(
+                      (String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                    hint: Text('Dropdown'),
+                    onChanged: (value) {
+                      setState(() {
+                        secdropDownValue = value!;
+                      });
+                    }),
               ],
-            )
-          ),
-        ],
-      )
-     )
-   );
+            )),
+            Column(children: [
+              ElevatedButton(
+                  child: Text(
+                    "Continue",
+                  ),
+                  onPressed: () {
+                    //navigateToConversionPage();
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ConversionPage()));
+                  }),
+            ])
+          ],
+        )));
   }
 }
