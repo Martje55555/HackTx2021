e6c11e27d066be65c7d87b7877f8d25f4a38c8e4
AlonsoGonzalezGonzalez <alonso.gonzalezgonzalez16@gmail.com>
2021-10-30 21:11:31 -0500

    images rip

--- lib/pages/conversion.dart
+++ lib/pages/conversion.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hack_tx_2021/global.dart';
+import '../global.dart';
 import './distance.dart';
 
 class ConversionPage extends StatefulWidget {
@@ -8,10 +8,32 @@ class ConversionPage extends StatefulWidget {
 }
 
 class _ConversionPage extends State<ConversionPage> {
+  double res = totalDistance(distance1, distance2);
+  String xd = url_sun;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(),
+      backgroundColor: Color(0xff4C748B),
+      appBar: AppBar(
+          backgroundColor: Colors.blue,
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DistancePage()));
+              }),
+          title: Text("Distance")),
+      body: Center(
+          child: Column(children: <Widget>[
+        Text(
+          '$res' + 'light years',
+          style: TextStyle(fontSize: 20),
+        ),
+        Image.network(
+          xd,
+          color: Colors.transparent,
+        ),
+      ])),
     );
   }
 }
